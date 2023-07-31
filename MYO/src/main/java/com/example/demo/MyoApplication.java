cfb14c6d764c431dcedd87ad5ad6a373d58286be
k252592 <xy20365.on@gmail.com>
2023-07-31 21:43:08 +0900

    main

---
+++ MYO/src/main/java/com/example/demo/MyoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyoApplication.class, args);
+	}
+
+}
